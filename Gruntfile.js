dedd3b0edaf740ea4ea4741cbbe8a9a624465423
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2016-02-24 21:58:05 +0100

    Grunt Deploy hinzufügen

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,20 @@
 module.exports = function(grunt) {
 
+	var deployFile = 'deploy_settings.json';
+	    deployInfo = {};
+
+	if (grunt.file.exists(deployFile)) {
+		deployInfo = grunt.file.readJSON(deployFile);
+	}
+
 	grunt.initConfig({
 		copy: {
 			main: {
 				expand: true,
 				cwd: 'src',
 				src: ['**', '!assets/css/**/*', '.htaccess'],
-				dest: 'dist/',
-			},
+				dest: 'dist/'
+			}
 		},
 
 		sass: {
@@ -76,6 +83,26 @@ module.exports = function(grunt) {
 				files: 'src/assets/img/**/*.{jpg,png}',
 				tasks: 'copy'
 			}
+		},
+
+		sshconfig: {
+			production: {
+				host:     deployInfo.host,
+				username: deployInfo.username,
+				password: deployInfo.password,
+				deployTo: deployInfo.deployTo
+			}
+		},
+
+		syncdeploy: {
+			main: {
+				cwd: 'dist/',
+				src: ['**/*', '.htaccess']
+			},
+			options: {
+				removeEmpty: true,
+				serverTimezone: 'GMT+0000'
+			}
 		}
 	});
 
@@ -85,11 +112,13 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sync-deploy');
 
 	grunt.option('config', 'production');
 
 	grunt.registerTask('build',      ['copy', 'sass', 'autoprefixer']);
 	grunt.registerTask('build-prod', ['build', 'htmlmin']);
+	grunt.registerTask('deploy',     ['build-prod', 'syncdeploy']);
 	grunt.registerTask('default',    ['build', 'watch']);
 
 };
